effa06572c371a6b42e1c07925313c9006bcb096
Water.Li <lizongbin@hotmail.com>
2021-05-18 20:21:54 +0800

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,9 +5,22 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  /**
+   * 通用查询接口，语法示例：
+   * {
+   *   "":{
+   *   }
+   * }
+   * @param json JSON格式的查询条件
+   * @returns 查询结果
+   */
   @Get('get/:json?')
-  getByJson(@Param('json') json): string {
+  getModels(@Param('json') json): string {
     return json;
-    //this.appService.getHello();
   }
 }
