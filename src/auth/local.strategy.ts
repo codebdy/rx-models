da4c1ff5f224afd799f4ab6ca7ddf6ba3afbcc47
Water.Li <lizongbin@hotmail.com>
2021-05-19 15:51:41 +0800

    Update local.strategy.ts

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -10,7 +10,6 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(username: string, password: string): Promise<any> {
-    console.log('validate', username, password);
     const user = await this.authService.validateUser(username, password);
     if (!user) {
       throw new UnauthorizedException();
