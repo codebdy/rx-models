f996a2243b2e83240a3d5e54dc4342d82f11a31c
Water.Li <lizongbin@hotmail.com>
2021-05-21 10:32:18 +0800

    move database config to ormconfig.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthController } from './auth.controller';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [TypeOrmModule.forRoot(), AuthModule, UsersModule],
   controllers: [AppController, AuthController],
   providers: [AppService],
 })
