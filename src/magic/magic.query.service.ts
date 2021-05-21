f0805228be4b32976b07cca33bf782b8f57b7d93
Water.Li <lizongbin@hotmail.com>
2021-05-21 19:00:44 +0800

    Update magic.query.service.ts

--- src/magic/magic.query.service.ts
+++ src/magic/magic.query.service.ts
@@ -2,14 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { User } from 'src/entity/User';
 import { getRepository } from 'typeorm';
 
-export type User = any;
-
 @Injectable()
 export class MagicQueryService {
-  private readonly users: User[];
-
-  constructor() {}
-
   async query(params: any) {
     return getRepository(User)
       .createQueryBuilder()
