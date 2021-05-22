1de2e7a21360484fc0ef58b5a4c784266de9cc27
Water.Li <lizongbin@hotmail.com>
2021-05-22 08:40:41 +0800

    Update magic.query.controller.ts

--- src/magic/query/magic.query.controller.ts
+++ src/magic/query/magic.query.controller.ts
@@ -28,7 +28,7 @@ export class MagicQueryController {
    *    "andWhere":{
    *      "xxx":"xxx"
    *    },
-   *    "roles @relationship @count @sum(ddd) as xx, @toUpcase(name)":{
+   *    "roles @relationship(Role) @count @sum(ddd) as xx, @toUpcase(name)":{
    *      "active": true,
    *      "isRemoved": false,
    *    },
