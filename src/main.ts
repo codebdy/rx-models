04c6476f890d39e148e37875df9a8d3a6db179de
Water.Li <lizongbin@hotmail.com>
2021-06-06 20:42:19 +0800

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({ credentials: true, origin: true });
+  await app.listen(3001);
 }
 bootstrap();
