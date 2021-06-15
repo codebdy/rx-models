837f22703b04807266e68ae6a3f3f98159ea5345
Water.Li <lizongbin@hotmail.com>
2021-06-15 10:20:51 +0800

    can resize image to thumbnail

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -1,75 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import { getRepository } from 'typeorm';
-import { EntityMeta } from '../magic/post/param/entity.meta';
-import { EntityMetaCollection } from '../magic/post/param/entity.meta.colletion';
-import { MagicPostParamsParser } from '../magic/post/param/post.param.parser';
-import { RelationMetaCollection } from '../magic/post/param/relation.meta.colletion';
+import * as sharp from 'sharp';
 
 @Injectable()
 export class UploadService {
-  async post(json: any) {
-    const savedEntites = {};
-    const entities = new MagicPostParamsParser(json).entityMetas;
-    for (const entityGroup of entities) {
-      savedEntites[entityGroup.model] = await this.saveEntityGroup(entityGroup);
-    }
-    return savedEntites;
-  }
-
-  private async saveEntityGroup(entityGroup: EntityMetaCollection) {
-    const savedEntites = [];
-
-    for (const entity of entityGroup.entites) {
-      savedEntites.push(await this.saveEntity(entity));
+  async saveThumbnail(file: any) {
+    const thumbnailPath = `./public/thumbnails/${file.filename}`;
+    if (file.mimetype.match(/\/(jpg|jpeg|png|gif)$/)) {
+      sharp(file.path)
+        .resize(200, 200)
+        .toFile(thumbnailPath, (err, info) => {
+          console.debug('Resize Success', info);
+          if (err) {
+            console.error('Resize Error', err);
+          }
+        });
+      console.debug('上传图像', file);
     }
 
-    return entityGroup.isSingleEntity ? savedEntites[0] : savedEntites;
-  }
-
-  private async proceRelationGroup(relationCollection: RelationMetaCollection) {
-    let savedEntites = [];
-
-    for (const entity of relationCollection.entites) {
-      savedEntites.push(await this.saveEntity(entity));
-    }
-
-    if (relationCollection.ids.length > 0) {
-      const repository = getRepository(relationCollection.model);
-      const relationEntities = await repository.findByIds(
-        relationCollection.ids,
-      );
-      savedEntites = savedEntites.concat(relationEntities);
-    }
-
-    return relationCollection.isSingleEntity ? savedEntites[0] : savedEntites;
-  }
-
-  private async saveEntity(entityMeta: EntityMeta) {
-    const relations = entityMeta.relations;
-    for (const relationKey in relations) {
-      const relationShip: RelationMetaCollection = relations[relationKey];
-      entityMeta.savedRelations[relationKey] =
-        relationShip.ids.length === 0
-          ? null
-          : await this.proceRelationGroup(relationShip);
-    }
-    const repository = getRepository(entityMeta.model);
-    let entity: any = repository.create();
-    if (entityMeta.meta?.id) {
-      entity = await repository.findOne(entityMeta.meta?.id);
-    }
-
-    for (const attrKey in entityMeta.meta) {
-      if (attrKey !== 'id') {
-        entity[attrKey] = entityMeta.meta[attrKey];
-      }
-    }
-
-    for (const relationKey in entityMeta.savedRelations) {
-      const relationValue = entityMeta.savedRelations[relationKey];
-      entity[relationKey] = relationValue;
-    }
-
-    return await repository.save(entity);
+    return thumbnailPath;
   }
 }
