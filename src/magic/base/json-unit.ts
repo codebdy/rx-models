abe0e8800f1aa9489859b147da819e9857758afc
Water.Li <lizongbin@hotmail.com>
2021-06-27 08:37:40 +0800

    remove useless code

--- src/magic/base/json-unit.ts
+++ src/magic/base/json-unit.ts
@@ -1,13 +1,4 @@
-import {
-  TOKEN_MODEL,
-  TOKEN_ON,
-  TOKEN_ORDER_BY,
-  TOKEN_PAGINATE,
-  TOKEN_SELECT,
-  TOKEN_SKIP,
-  TOKEN_TAKE,
-  TOKEN_WHERE,
-} from './tokens';
+import { TOKEN_MODEL } from './tokens';
 import { parseCommands } from './parse-commands';
 import { CommandMeta } from 'src/command/command.meta';
 
@@ -24,7 +15,7 @@ export class JsonUnit {
 
   getCommand(commandName: string) {
     for (const command of this.commands) {
-      if (command.name?.toLowerCase() === commandName.toLowerCase()) {
+      if (command.name === commandName) {
         return command;
       }
     }
@@ -33,33 +24,5 @@ export class JsonUnit {
 
   isModel() {
     return this.key.toLowerCase() === TOKEN_MODEL;
-  }
-
-  isSelect() {
-    return this.key?.toLowerCase() === TOKEN_SELECT.toLowerCase();
-  }
-
-  isOrderBy() {
-    return this.key?.toLowerCase() === TOKEN_ORDER_BY.toLowerCase();
-  }
-
-  isWhere() {
-    return this.key?.toLowerCase() === TOKEN_WHERE.toLowerCase();
-  }
-
-  isOn() {
-    return this.key?.toLowerCase() === TOKEN_ON.toLowerCase();
-  }
-
-  getTakeCommand() {
-    return this.getCommand(TOKEN_TAKE);
-  }
-
-  getSkipCommand() {
-    return this.getCommand(TOKEN_SKIP);
-  }
-
-  getPaginateCommand() {
-    return this.getCommand(TOKEN_PAGINATE);
   }
 }
