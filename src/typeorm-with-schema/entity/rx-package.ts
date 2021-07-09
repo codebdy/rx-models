c29a05eb06b51cef6cbda3c6659d4aa2068a720a
Water.Li <lizongbin@hotmail.com>
2021-07-09 14:02:00 +0800

    Create rx-package.ts

---
+++ src/typeorm-with-schema/entity/rx-package.ts
@@ -0,0 +1,42 @@
+import { EntitySchema } from 'typeorm';
+
+export const UserEntity = new EntitySchema<any>({
+  name: 'RxPackage',
+  columns: {
+    id: {
+      type: Number,
+      primary: true,
+      generated: true,
+    },
+    uuid: {
+      type: Number,
+    },
+    name: {
+      type: String,
+    },
+    entities: {
+      type: 'simple-json',
+      nullable: true,
+    },
+    diagrams: {
+      type: 'simple-json',
+      nullable: true,
+    },
+    relations: {
+      type: 'simple-json',
+      nullable: true,
+    },
+    status: {
+      type: String,
+      default: 'EDITING', //'EDITING' | 'SYNCED'
+    },
+    createdAt: {
+      type: Date,
+      createDate: true,
+    },
+    updatedAt: {
+      type: Date,
+      createDate: true,
+    },
+  },
+});
