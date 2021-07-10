2d985ce0f0695406508b19ae686f6e79ac4c263c
Water.Li <lizongbin@hotmail.com>
2021-07-10 12:00:22 +0800

    small change

--- src/meta/entity/column-meta.ts
+++ src/meta/entity/column-meta.ts
@@ -37,41 +37,9 @@ export interface ColumnMeta {
    * of digits to the right of the decimal point and must not be greater than precision.
    */
   scale?: number;
-  /**
-   * Puts ZEROFILL attribute on to numeric column. Works only for MySQL.
-   * If you specify ZEROFILL for a numeric column, MySQL automatically adds the UNSIGNED attribute to the column
-   */
-  zerofill?: boolean;
-  /**
-   * Puts UNSIGNED attribute on to numeric column. Works only for MySQL.
-   */
-  unsigned?: boolean;
-  /**
-   * Defines a column character set.
-   * Not supported by all database types.
-   */
-  charset?: string;
+
   /**
    * Defines a column collation.
    */
-  collation?: string;
-  /**
-   * Generated column expression. Supports only in MySQL.
-   */
-  asExpression?: string;
-  /**
-   * Generated column type. Supports only in MySQL.
-   */
-  generatedType?: 'VIRTUAL' | 'STORED';
-  /**
-   * Return type of HSTORE column.
-   * Returns value as string or as object.
-   */
-  hstoreType?: 'object' | 'string';
-  /**
-   * Indicates if this column is an array.
-   * Can be simply set to true or array length can be specified.
-   * Supported only by postgres.
-   */
-  array?: boolean;
+  //collation?: string;
 }
