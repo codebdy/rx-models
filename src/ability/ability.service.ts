87c138f07e321f532ddf2a794f411076b69889b6
Water.Li <lizongbin@hotmail.com>
2021-07-14 21:09:43 +0800

    Update ability.service.ts

--- src/ability/ability.service.ts
+++ src/ability/ability.service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Scope } from '@nestjs/common';
 import { RxAbility } from 'src/entity-interface/rx-ability';
+import { REQUEST } from '@nestjs/core';
+import { Request } from 'express';
+import { TypeOrmService } from 'src/typeorm/typeorm.service';
 
-@Injectable()
+@Injectable({ scope: Scope.REQUEST })
 export class AbilityService {
+  constructor(
+    @Inject(REQUEST) private readonly request: Request,
+    private readonly typeormSerivce: TypeOrmService,
+  ) {}
+
   getEntityReadAbility(): RxAbility | undefined {
     return undefined;
   }
