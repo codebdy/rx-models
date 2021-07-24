1a4bf324785e543bcb7d143ddc4a6840f9fab0b3
Water.Li <lizongbin@hotmail.com>
2021-07-24 23:47:04 +0800

    Update auth.service.ts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -23,6 +23,7 @@ export class AuthService {
       .addSelect('user.password')
       .where({ loginName: username })
       .getOne()) as RxUser;
+
     if (user && (await bcrypt.compare(pass, user.password))) {
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
       const { password, ...result } = user;
