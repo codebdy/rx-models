118ff7bad974b3732706facf846329d5d6d155d8
Water.Li <lizongbin@hotmail.com>
2021-09-19 14:09:20 +0800

    Update query-condition-equal.ts

--- src/directives/query-condition-equal.ts
+++ src/directives/query-condition-equal.ts
@@ -14,7 +14,7 @@ export class QueryConditionEqualDirective extends QueryConditionDirective {
   getWhereStatement(): [string, any] {
     const paramName = 'param' + createId();
     let paramValue = this.value;
-    if (this.value.toString().startsWith('$me.')) {
+    if (this.value?.toString().startsWith('$me.')) {
       const [, columnStr] = (this.value as string).split('.');
       paramValue = this.magicService.me[columnStr];
     }
