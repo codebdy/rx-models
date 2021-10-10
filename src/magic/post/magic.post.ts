69c091ee771d7cf401fd560ae0730e0888aac428
Water.Li <lizongbin@hotmail.com>
2021-10-10 13:28:52 +0800

    Update magic.post.ts

--- src/magic/post/magic.post.ts
+++ src/magic/post/magic.post.ts
@@ -170,9 +170,6 @@ export class MagicPost {
     for (const key in instanceMeta.relations) {
       filteredInstance[key] = inststance[key];
     }
-
-    console.log(filteredInstance, instanceMeta);
-
     return filteredInstance;
   }
 
