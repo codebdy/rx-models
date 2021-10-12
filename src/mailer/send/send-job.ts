206176364fcd0103c83603c08f56625dcda92303
Water.Li <lizongbin@hotmail.com>
2021-10-12 19:03:44 +0800

    backup

--- src/mailer/send/send-job.ts
+++ src/mailer/send/send-job.ts
@@ -1,8 +1,19 @@
+import { AddressItem } from 'src/entity-interface/AddressItem';
 import { IJob } from '../job/i-job';
 import { JobOwner } from '../job/job-owner';
 
+enum SendStatus {
+  finished = 'finished',
+  sending = 'sending',
+  error = 'error',
+}
+
+type AddressItemWithStatus = AddressItem & {
+  status?: SendStatus;
+};
 export class SendJob implements IJob {
   jobOwner: JobOwner;
+  to?: AddressItemWithStatus;
 
   start(): void {
     throw new Error('Method not implemented.');
