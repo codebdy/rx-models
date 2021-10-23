7cf28c501e4d55bf4616a78d2b69abc7c4ee4222
Water.Li <lizongbin@hotmail.com>
2021-10-23 14:04:32 +0800

    Update receive-job.ts

--- src/mailer/receive/receive-job.ts
+++ src/mailer/receive/receive-job.ts
@@ -182,7 +182,7 @@ export abstract class ReceiveJob implements IReceiveJob {
       message: 'Recevied one mail',
       subject: passedMail.subject,
       inMailbox: mailBox,
-    })
+    });
   }
 
   continue(): void {
